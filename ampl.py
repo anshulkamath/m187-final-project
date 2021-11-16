828baa850a69a7f496304c639f6914e3805f780c
Claire Chang <claires.chang@gmail.com>
2021-11-16 02:23:34 -0800

    modify heatmap generation

--- ampl.py
+++ ampl.py
@@ -9,8 +9,8 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # setup
-ampl = AMPL(Environment('../../'))
-ampl.setOption('solver', '../../gurobi')
+ampl = AMPL(Environment('../ampl_mswin64/ampl_mswin64/'))
+ampl.setOption('solver', '../ampl_mswin64/ampl_mswin64/gurobi')
 ampl.setOption('solver_msg', 0)
 ampl.setOption('outlev', 0)
 
@@ -19,78 +19,36 @@ def solve_model():
     ampl.reset()
     ampl.read('ampl-files/case-study-2.mod')
     ampl.readData('ampl-files/case-study-2.dat')
-    ampl.eval('solve >/dev/null;')
+#     ampl.eval('solve >/dev/null;')
+    ampl.solve()
 
-def create_df(ampl_output, prefs):
-    df_dict = {}
-    cols = sorted([section for c in ent.classes for section in c.get_sections()])
+
+def create_matrix(ampl_output, prefs):
+    num_sections = len([section for c in ent.classes for section in c.get_sections()])
+
+    # number of people for each level of happiness
+    req_happiness, elect_happiness = [0,0,0,0], [0,0,0,0]
 
     # create dictionary
-    for (student, _, _, is_taking) in ampl_output:
-        student = round(float(student))
-
-        if student not in df_dict:
-            df_dict[student] = []
+    for i in range(0, len(ampl_output), num_sections):
+        student = ampl_output[i][0]
+        curr_prefs = prefs[int(student)]['preferences']
+        class_idx = np.where(np.array(ampl_output[i:i+num_sections])[:,3] == '1.0')
         
-        df_dict[student].append(is_taking)
+        if len(class_idx[0]) == 1:  # assume it's an elective
+            elect_happiness[curr_prefs[class_idx[0][0]]] += 1
+        else:  # assume 1 req and 2 elect preplacement, and req comes first in list
+            req_happiness[curr_prefs[class_idx[0][0]]] += 1
+            elect_happiness[curr_prefs[class_idx[0][1]]] += 1
 
-    # convert dictionary to numpy and take product with prefs
-    for key in df_dict:
-        curr_prefs = prefs[key]['preferences']
+    return np.array(req_happiness).reshape((-1,1)) + np.array(elect_happiness).reshape((1,-1))
 
-        # normalize wrt the highest 
-        df_dict[key] = np.array(df_dict[key]) * np.array(curr_prefs) / max(curr_prefs)
 
-    df = DataFrame.from_dict(df_dict, orient='index', dtype=np.uint16, columns=cols)
-
-    return df
-
-def get_heatmap_data(df: DataFrame):
-    students = []
-    data_low = {}
-    data_high = {}
-
-    for row in df.index:
-        row = df.loc[row].to_numpy()
-        appendee = sorted(row[np.nonzero(row)], reverse=True)
-
-        if len(appendee) == 1:
-            appendee.append(0)
-            appendee.reverse()
-
-        students.append(appendee if appendee else [0, 0])
-    
-    for [low, high] in students:
-        if low not in data_low:
-            data_low[low] = 0
-        data_low[low] += 1
-
-        if high not in data_high:
-            data_high[high] = 0
-        data_high[high] += 1
-
-        # enforcing consistent shapes
-        if low not in data_high:
-            data_high[low] = 0
-
-        if high not in data_low:
-            data_low[high] = 0
-
-    ret = []; labels = []
-    for data in [data_low, data_high]:
-        acc_labels = []
-        acc_ret = []
-        for key in sorted(data.keys()):
-            acc_labels.append(key)
-            acc_ret.append(data[key])
-        
-        labels.append(acc_labels)
-        ret.append(acc_ret)
-
-    return DataFrame(ret, columns=labels[0], index=labels[1])
 
 def create_heatmap(data: DataFrame):
-    ax = sns.heatmap(data)
+    ax = sns.heatmap(data, cmap='BuPu', cbar_kws={'label': 'Number of Students'})
+    plt.xlabel("Happiness with Required Class")
+    plt.ylabel("Happiness with Elective Class")
     plt.show()
 
 for i in range(1):
@@ -99,5 +57,5 @@ for i in range(1):
     solve_model()
 
     x_soln = ampl.getData('x;').toList()
-    df = create_df(x_soln, prefs)
-    create_heatmap(get_heatmap_data(df))
+    matrix = create_matrix(x_soln, prefs)
+    create_heatmap(matrix)
